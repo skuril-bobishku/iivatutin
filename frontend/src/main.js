1c266b88e552b9de42677dca1d335de3aaeda2fa
skuril-bobishku <e19.e71@bk.ru>
2024-12-15 22:08:54 +0300

    combine back + front (parser)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index.js';
 
-createApp(App).use(router).mount('#app');
+import '@/assets/parser.css';
+import '@/assets/parse-list.css';
+import '@/assets/parse-input.css';
+import '@/assets/parse-config.css';
+import '@/assets/parse-buttons.css';
+
+//createApp(App).use(router).mount('#app');
+createApp(App).mount('#app');
